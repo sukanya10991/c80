1cc64a94d183d2d93968ccaa18d85854c7e54c08
sukanya10991 <78801848+sukanya10991@users.noreply.github.com>
2021-04-27 14:00:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 function get(){
-    var joins=[];
-    for(var s=0;s<=8;s++){
-        joins.push(document.getElementById("1a")+i.value);
-        joins.push(document.getElementById("2a")+i.value);
-        joins.push(document.getElementById("3a")+i.value);
-        joins.push(document.getElementById("4a")+i.value);
-        joins.push(document.getElementById("1b")+i.value);
-        joins.push(document.getElementById("2b")+i.value);
-        joins.push(document.getElementById("3b")+i.value);
-        joins.push(document.getElementById("4b")+i.value);
+    var joins=[];    
+        joins.push(document.getElementById("1a").value);
+        joins.push(document.getElementById("2a").value);
+        joins.push(document.getElementById("3a").value);
+        joins.push(document.getElementById("4a").value);
+        joins.push(document.getElementById("1b").value);
+        joins.push(document.getElementById("2b").value);
+        joins.push(document.getElementById("3b").value);
+        joins.push(document.getElementById("4b").value);
         joins.join(". ");
         console.log("joins");
         document.getElementById("ab").innerHTML=joins;        
         document.getElementById("ab").innerHTML = inputs.join(". ");
-    }
+    
 }
